b39df8de6febd7063d5dca18b78bd908b4d51a85
Yann Braga <yannbf@gmail.com>
2017-09-19 14:06:27 -0300

    chore(home): Add movie component + refactor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,15 +7,28 @@ import { ApiConnectorProvider } from '../../providers/api-connector/api-connecto
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // MOCK DATA FOR TESTING PURPOSES
+  ceara     = [{ "id": "36", "name": "Fortaleza", "uf": "CE", "state": "Ceará", "urlKey": "fortaleza", "timeZone": "America/Fortaleza" }, { "id": "450", "name": "Juazeiro do Norte", "uf": "CE", "state": "Ceará", "urlKey": "juazeiro-do-norte", "timeZone": "America/Fortaleza" }, { "id": "312", "name": "Maracanaú", "uf": "CE", "state": "Ceará", "urlKey": "maracanau", "timeZone": "America/Fortaleza" }, { "id": "455", "name": "Maranguape", "uf": "CE", "state": "Ceará", "urlKey": "maranguape", "timeZone": "America/Sao_Paulo" }];
+  fortaleza = { "id": "36", "name": "Fortaleza", "uf": "CE", "state": "Ceará", "urlKey": "fortaleza", "timeZone": "America/Fortaleza" };
+  theater   = 1256;
+
+  movies;
   states;
-  
+
   constructor(public navCtrl: NavController, public apiConnector: ApiConnectorProvider) {
-    let statePromise = apiConnector.loadStates();
-    statePromise.subscribe(
-      data => {
-        this.states = data
-      }
-    );
+    // this.loadStatesData();
+    this.loadMovies();
   }
 
+  loadMovies() {
+    this.apiConnector
+        .getMovies(this.fortaleza.id, this.theater)
+        .subscribe(data => this.movies = data[0].movies);
+  }
+
+  loadStatesData() {
+    this.apiConnector
+        .loadStates()
+        .subscribe(data => this.states = data);
+  }
 }
