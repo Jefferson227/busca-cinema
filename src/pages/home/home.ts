c623d29be14d2bf2fa3532b10a45cd2ca13cd0c9
Jefferson Matheus <jefferson227@gmail.com>
2017-09-19 22:55:39 -0300

    Remove mock from fakemovie file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ApiConnectorProvider } from '../../providers/api-connector/api-connector';
-import { FAKE_MOVIE_DATA } from "./fakemovie.data";
 
 @Component({
   selector: 'page-home',
@@ -9,12 +8,9 @@ import { FAKE_MOVIE_DATA } from "./fakemovie.data";
 })
 export class HomePage {
   // MOCK DATA FOR TESTING PURPOSES
-  ceara     = [{ "id": "36", "name": "Fortaleza", "uf": "CE", "state": "Ceará", "urlKey": "fortaleza", "timeZone": "America/Fortaleza" }, { "id": "450", "name": "Juazeiro do Norte", "uf": "CE", "state": "Ceará", "urlKey": "juazeiro-do-norte", "timeZone": "America/Fortaleza" }, { "id": "312", "name": "Maracanaú", "uf": "CE", "state": "Ceará", "urlKey": "maracanau", "timeZone": "America/Fortaleza" }, { "id": "455", "name": "Maranguape", "uf": "CE", "state": "Ceará", "urlKey": "maranguape", "timeZone": "America/Sao_Paulo" }];
-  fortaleza = { "id": "36", "name": "Fortaleza", "uf": "CE", "state": "Ceará", "urlKey": "fortaleza", "timeZone": "America/Fortaleza" };
-  theater   = 1256;
-
-  movies = FAKE_MOVIE_DATA;
-
+  cityId    = 36;
+  theaterId = 1256;
+  movies;
   states;
 
   constructor(public navCtrl: NavController, public apiConnector: ApiConnectorProvider) {
@@ -24,7 +20,7 @@ export class HomePage {
 
   loadMovies() {
     this.apiConnector
-        .getMovies(this.fortaleza.id, this.theater)
+        .getMovies(this.cityId, this.theaterId)
         .subscribe(data => {
           this.movies = data[0].movies;
         });
