0d371710585246025b991fe28be5a9367c813e55
Jefferson Matheus <jefferson227@gmail.com>
2017-09-19 22:53:35 -0300

    Configure proxy for api

--- src/providers/api-connector/api-connector.ts
+++ src/providers/api-connector/api-connector.ts
@@ -4,20 +4,18 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ApiConnectorProvider {
-  baseUrl = 'https://api-content.ingresso.com/v0';
-
   constructor(public http: Http) {
   }
 
   loadStates() {
     return this.http
-      .get(this.baseUrl + '/states')
+      .get('/api/states')
       .map(res => res.json());
   }
 
   getMovies(cityId, theaterId) {
     return this.http
-    .get(`${this.baseUrl}/sessions/city/${cityId}/theater/${theaterId}`)
+    .get(`/api/sessions/city/${cityId}/theater/${theaterId}`)
     .map(res => res.json());
   }
 
