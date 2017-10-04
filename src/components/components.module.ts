90cf527c052621ad939007b21eed841e403be197
Jefferson Matheus <jefferson227@gmail.com>
2017-10-04 15:00:39 -0300

    Create component session-information

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MovieComponent } from './movie/movie';
 import { IonicModule } from "ionic-angular";
+import { SessionInformationComponent } from './session-information/session-information';
 @NgModule({
-	declarations: [MovieComponent],
+	declarations: [MovieComponent,
+    SessionInformationComponent],
 	imports: [IonicModule],
-	exports: [MovieComponent]
+	exports: [MovieComponent,
+    SessionInformationComponent]
 })
 export class ComponentsModule {}
