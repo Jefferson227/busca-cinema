f34d4191827e6aa44f1b94d3ec1b1e47074e2a15
Jefferson Matheus <jefferson227@gmail.com>
2017-10-15 09:19:32 -0300

    Migrate api calls to heroku

--- src/providers/api-connector/api-connector.ts
+++ src/providers/api-connector/api-connector.ts
@@ -4,55 +4,27 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ApiConnectorProvider {
-  baseUrl = '/api';
-  //baseUrl = 'https://api-content.ingresso.com/v0';
+  //baseUrl = '/api';
+  baseUrl = 'https://busca-cinema-backend.herokuapp.com';
 
   constructor(public http: Http) {
   }
 
-  loadStates() {
-    return this.http
-      .get(`${this.baseUrl}/states`)
-      .map(res => res.json());
-  }
-
   getMovies(cityId) {
-    let partnership = 0;
-
     return this.http
-      .get(`${this.baseUrl}/events/city/${cityId}/partnership/${partnership}`)
+      .get(`${this.baseUrl}/events/city/${cityId}`)
       .map(res => res.json());
   }
 
   getMovieDetail(id) {
-    let partnership = 0;
-
     return this.http
-      .get(`${this.baseUrl}/events/${id}/partnership/${partnership}`)
-      .map(res => res.json());
-  }
-
-  getTheaters(cityId) {
-    let partnership = 0;
-
-    return this.http
-      .get(`${this.baseUrl}/theaters/city/${cityId}/partnership/${partnership}`)
-      .map(res => res.json());
-  }
-
-  getMoviesByTheater(cityId, theaterId, date) {
-    let partnership = 0;
-
-    return this.http
-      .get(`${this.baseUrl}/sessions/city/${cityId}/theater/${theaterId}/partnership/${partnership}?date=${date}`)
+      .get(`${this.baseUrl}/events/${id}`)
       .map(res => res.json());
   }
 
   getTheatersByMovie(cityId, movieId, date) {
-    let partnership = 0;
-
     return this.http
-      .get(`${this.baseUrl}/sessions/city/${cityId}/event/${movieId}/partnership/${partnership}?date=${date}`)
+      .get(`${this.baseUrl}/sessions/city/${cityId}/event/${movieId}/date/${date}`)
       .map(res => res.json());
   }
 }
