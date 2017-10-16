783caf619fb300327a87517fc5dae7e2d0fd99af
Yann Braga <yannbf@gmail.com>
2017-10-16 16:50:01 -0300

    feat(api): Add possibility to cache requests

--- src/providers/api-connector/api-connector.ts
+++ src/providers/api-connector/api-connector.ts
@@ -1,25 +1,26 @@
 import { ApiProvider } from '../api/api';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CacheService } from "ionic-cache";
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ApiConnectorProvider extends ApiProvider {
 
-  constructor(public http: HttpClient) {
-    super(http, 'https://busca-cinema-backend.herokuapp.com');
+  constructor(public http: HttpClient, public cache: CacheService) {
+    super('https://busca-cinema-backend.herokuapp.com', http, cache);
   }
 
   getMovies(cityId) {
-    return this.get(`events/city/${cityId}`);
+    return this.get(`events/city/${cityId}`, {cache: true});
   }
 
   getMovieDetail(id) {
-    return this.get(`events/${id}`);
+    return this.get(`events/${id}`, {cache: true});
   }
 
   getTheatersByMovie(cityId, movieId, date) {
-    return this.get(`sessions/city/${cityId}/event/${movieId}/date/${date}`);
+    return this.get(`sessions/city/${cityId}/event/${movieId}/date/${date}`, {cache: true});
   }
 
 }
