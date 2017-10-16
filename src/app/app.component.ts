bdd34345ac4624f0e21d92839bf090034d91a938
Yann Braga <yannbf@gmail.com>
2017-10-16 16:49:10 -0300

    feat(app): Add cache service initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CacheService } from "ionic-cache";
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,10 +11,11 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, cache: CacheService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      cache.setDefaultTTL(60 * 20); //set default cache TTL for 20 minutes
       statusBar.styleDefault();
       splashScreen.hide();
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { CacheModule } from "ionic-cache";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -22,6 +23,7 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(MyApp),
+    CacheModule.forRoot(),
     ComponentsModule,
   ],
   bootstrap: [IonicApp],
