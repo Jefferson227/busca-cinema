518dd9e5a0d18574cd9da8d6b51e717b30f789f6
Jefferson Matheus <jefferson227@gmail.com>
2017-11-26 09:44:29 -0300

    chore(Home): implement localization
    Implement the code to get the localization by gps or typing the name of
    the city on the modal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,89 +11,107 @@ import { LocationPage } from "../location/location";
   templateUrl: 'home.html'
 })
 export class HomePage {
-  // MOCK DATA FOR TESTING PURPOSES
-  movies: any[];
-  loading: any;
+    movies: any[];
+    loading: any;
+    city: string;
 
-  constructor(public navCtrl: NavController,
-      public apiConnector: ApiConnectorProvider,
-      public loadingProvider: LoadingProvider,
-      private geolocation: Geolocation,
-      private modalCtrl: ModalController
-    ) {
-    this.loading = this.loadingProvider.initialize();
-    this.loadingProvider.show(this.loading);
-    this.geolocation.getCurrentPosition().then((resp) => {
-      console.log(resp);
-      this.getCityByLocation(resp.coords.latitude, resp.coords.longitude);
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });
-    this.loadMovies();
-    this.modalCtrl.create(LocationPage).present();
-  }
-
-  loadMovies() {
-    this.apiConnector
-        .getMovies()
-        .subscribe((data: any) => {
-          this.movies = data;
-          this.loadingProvider.hide(this.loading);
-        });
-  }
-
-  getCityByLocation(lat, long) {
-    this.apiConnector
-    .getCityByLocation(lat, long)
-    .subscribe((position: any) => {
-      console.log(this.getCityNameByCoordinates(position));
-    });
-  }
-
-  getCityNameByCoordinates(position) {
-    if (position.results.length) {
-
-      let firstPosition = position.results[0];
-
-      let cityName = firstPosition.address_components.map((i) => {
-          if (i.types.includes('locality')) {
-              return i;
-          }
-      })
-      .filter((f) => f !== undefined)[0].long_name;
-
-      let state = firstPosition.address_components.map((i) => {
-          if (i.types.includes('administrative_area_level_1')) {
-              return i;
-          }
-      })
-      .filter((f) => f !== undefined)[0].long_name;
-
-      let country = firstPosition.address_components.map((i) => {
-          if (i.types.includes('country')) {
-              return i;
-          }
-      })
-      .filter((f) => f !== undefined)[0].long_name;
-
-      return `${cityName}, ${state} - ${country}`;
+    constructor(public navCtrl: NavController,
+        public apiConnector: ApiConnectorProvider,
+        public loadingProvider: LoadingProvider,
+        private geolocation: Geolocation,
+        private modalCtrl: ModalController
+      ) {
+      this.loading = this.loadingProvider.initialize();
+      this.loadingProvider.show(this.loading);
+      this.getLocation();
+      this.loadMovies();
     }
 
-    return 'Couldn\'t get the city name';
-  }
+    loadMovies() {
+      this.apiConnector
+          .getMovies()
+          .subscribe((data: any) => {
+            this.movies = data;
+            this.loadingProvider.hide(this.loading);
+          });
+    }
 
-  goToDetail(movieId) {
-    console.log(`Movie ${movieId} clicked`);
-    return;
-    // this.apiConnector
-    //   .getMovieDetail(movieId)
-    //   .subscribe(data => {
-    //     let objParams = {
-    //       cityId: this.cityId,
-    //       movie: data
-    //     };
+    getCityByLocation(lat: number, long: number): void {
+      this.apiConnector
+        .getCityByLocation(lat, long)
+        .subscribe((position: any) => {
+          let city: string = this.getCityNameByCoordinates(position);
 
-    //     this.navCtrl.push(MovieDetailPage, objParams);
-    //   });
-  }
+          if (city) {
+            localStorage.setItem('location', city);
+            this.city = city;
+          }
+          else {
+            this.modalCtrl.create(LocationPage).present();
+          }
+        });
+    }
+
+    getCityNameByCoordinates(position): string {
+      if (position.results.length) {
+
+        let firstPosition = position.results[0];
+
+        let cityName = firstPosition.address_components.map((i) => {
+            if (i.types.includes('locality')) {
+                return i;
+            }
+        })
+        .filter((f) => f !== undefined)[0].long_name;
+
+        let state = firstPosition.address_components.map((i) => {
+            if (i.types.includes('administrative_area_level_1')) {
+                return i;
+            }
+        })
+        .filter((f) => f !== undefined)[0].long_name;
+
+        let country = firstPosition.address_components.map((i) => {
+            if (i.types.includes('country')) {
+                return i;
+            }
+        })
+        .filter((f) => f !== undefined)[0].long_name;
+
+        return `${cityName}, ${state} - ${country}`;
+      }
+
+      return '';
+    }
+
+    getLocation(): void {
+      let city: string = localStorage.getItem('location');
+
+      if (city) {
+        this.city = city;
+      }
+      else {
+        this.geolocation.getCurrentPosition().then((resp) => {
+          this.getCityByLocation(resp.coords.latitude, resp.coords.longitude);
+        }).catch((error) => {
+          this.modalCtrl.create(LocationPage).present();
+          console.error('Error on getting location', error);
+        });
+      }
+    }
+
+    goToDetail(movieId) {
+      console.log(`Movie ${movieId} clicked`);
+      return;
+      // this.apiConnector
+      //   .getMovieDetail(movieId)
+      //   .subscribe(data => {
+      //     let objParams = {
+      //       cityId: this.cityId,
+      //       movie: data
+      //     };
+
+      //     this.navCtrl.push(MovieDetailPage, objParams);
+      //   });
+    }
 }
