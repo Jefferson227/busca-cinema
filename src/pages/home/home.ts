df915cda0e65cfe253f95966a135179bcbc221aa
Jefferson Matheus <jefferson227@gmail.com>
2017-12-17 20:30:53 -0300

    refactor(Home): filter movie details

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,23 +7,40 @@ import { ApiConnectorProvider } from '../../providers/api-connector/api-connecto
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // Variables
   movies: any[];
   moviePromises: any[];
+  baseUrlImage: string = 'https://image.tmdb.org/t/p/w500';
 
   constructor(public navCtrl: NavController, private _apiConnector: ApiConnectorProvider) {
+    // Initializing arrays
     this.movies = [];
     this.moviePromises = [];
 
+    // Getting all movies
     this._apiConnector.getMovies()
       .subscribe(movies => {
+        // For each movies, getting its respecitve detail
         movies.forEach((movie) => {
           this.moviePromises.push(
+
             this._apiConnector.getMovieDetails(movie.name)
-              .subscribe(movieDetails => this.movies.push(movieDetails))
+              .subscribe(movieDetails => {
+
+                if (movieDetails && movieDetails.results.length) {
+                  let details = movieDetails.results[0];
+                  details.backdrop_fullpath = `${this.baseUrlImage}${details.backdrop_path}`;
+                  details.poster_fullpath = `${this.baseUrlImage}${details.poster_path}`;
+
+                  this.movies.push(details);
+                }
+
+              })
+
           );
 
-          Promise.all(this.moviePromises)
-            .then(moviePromise => console.log(moviePromise));
+          // Executing all promises with movie details
+          Promise.all(this.moviePromises);
         });
       });
   }
