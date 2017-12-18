2b776419f46dd7fbfad46e5e1c82394304bf56e0
Jefferson Matheus <jefferson227@gmail.com>
2017-12-17 23:50:52 -0300

    refactor(Home): improve the code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,38 +9,28 @@ import { ApiConnectorProvider } from '../../providers/api-connector/api-connecto
 export class HomePage {
   // Variables
   movies: any[];
-  moviePromises: any[];
   baseUrlImage: string = 'https://image.tmdb.org/t/p/w500';
 
   constructor(public navCtrl: NavController, private _apiConnector: ApiConnectorProvider) {
     // Initializing arrays
     this.movies = [];
-    this.moviePromises = [];
 
     // Getting all movies
     this._apiConnector.getMovies()
       .subscribe(movies => {
         // For each movies, getting its respecitve detail
         movies.forEach((movie) => {
-          this.moviePromises.push(
+          this._apiConnector.getMovieDetails(movie.name)
+            .subscribe(movieDetails => {
+              if (movieDetails && movieDetails.results.length) {
+                let details = movieDetails.results[0];
 
-            this._apiConnector.getMovieDetails(movie.name)
-              .subscribe(movieDetails => {
+                details.backdrop_fullpath = `${this.baseUrlImage}${details.backdrop_path}`;
+                details.poster_fullpath = `${this.baseUrlImage}${details.poster_path}`;
 
-                if (movieDetails && movieDetails.results.length) {
-                  let details = movieDetails.results[0];
-                  details.backdrop_fullpath = `${this.baseUrlImage}${details.backdrop_path}`;
-                  details.poster_fullpath = `${this.baseUrlImage}${details.poster_path}`;
-
-                  this.movies.push(details);
-                }
-
-              })
-
-          );
-
-          // Executing all promises with movie details
-          Promise.all(this.moviePromises);
+                this.movies.push(details);
+              }
+            });
         });
       });
   }
